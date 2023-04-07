1532f75ddc2c519f01ed1c63a2080fbd9a7bc4e2
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-04-07 20:13:27 +0530

    Game BackGround Image SetUp

--- src/prakhar17/developer/fightgame/screens/Board.java
+++ src/prakhar17/developer/fightgame/screens/Board.java
@@ -2,12 +2,28 @@ package prakhar17.developer.fightgame.screens;
 
 import prakhar17.developer.fightgame.utils.GameConstants;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Board extends JPanel implements GameConstants {
+    BufferedImage backgroundImage;
 
-    public Board() {}
+    public Board() {
+        loadBackgroundImage();
+    }
+
+    private void loadBackgroundImage() {
+        try {
+            backgroundImage = ImageIO.read(Board.class.getResource("night_war_background.jpeg"));
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(this, "Unable to load background...");
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
 
     @Override
     public void paintComponent(Graphics pen) { //Automatically Called //Part Of Life Cycle
@@ -16,8 +32,6 @@ public class Board extends JPanel implements GameConstants {
     }
 
     private void printBG(Graphics pen) {
-        pen.setColor(Color.red);
-        pen.fillRect(0, 0, GW, GH);
-        System.out.println("prakhar");
+        pen.drawImage(backgroundImage, 0, 0, GW, GH, null);
     }
 }
